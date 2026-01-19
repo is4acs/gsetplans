388171ff0902871b4c2e5af8906a3556cd1c53dc
is4acs <Isaac.settou@yahoo.fr>
2026-01-19 11:25:42 +0100

    &

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // On supprime resolve, build.commonjsOptions et optimizeDeps qui causaient l'erreur
 })
